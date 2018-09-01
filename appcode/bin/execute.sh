d2aa2d3dde91b73996db6859e8883f619ed39a3e
lowemedia <andylowe@low-emedia.com>
2018-09-01 18:37:15 +0100

    Updated docker file to test container

--- appcode/bin/execute.sh
+++ appcode/bin/execute.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-exec /usr/local/bin/php /applications/hal-cli-v2/bin/console app:post-article-twitter >> /applications/hal-cli-v2/cron.log
+#exec /usr/local/bin/php /applications/hal-cli-v2/bin/console app:post-article-twitter >> /applications/hal-cli-v2/cron.log
+exec php-fpm
